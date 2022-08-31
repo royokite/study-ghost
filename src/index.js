24cbdafd35570c6649bd7ebd3fa4a8d006120503
RoyOkite <royokite@gmail.com>
2022-08-31 19:03:14 +0300

    Structure and Layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 document.addEventListener('DOMContentLoaded', runProject());
+
+function renderProducts(movie) {
+    const movieSpace = document.querySelector('#products');
+    
+    let card = document.createElement('li');
+    card.className = 'card';
+    card.innerHTML = `
+    <img src='${movie.image}' class='product-image'/>
+    <section class='card-content'>
+    <article class='box1'><h3>${movie.title}</h3></article>
+    <article class='box2'><h3>${movie.original_title}</h3></article>
+    <article class='box3'>${movie.director}</article>
+    <article class='box4'>${movie.producer}</article>
+    <article class='box5'><p>${movie.description}</p></article>
+    <article class='box6'>${movie.release_date}</article>
+    <article class='box7'>${movie.running_time}</article>
+    <article class='box8'>${movie.rt_score}</article>
+    <button class='box9'>Favourites</button>
+    <button class='box10'>Comment</button>
+    <button class='box11'>Rent</button>     
+    </section>
+    `;
+
+    movieSpace.appendChild(card);
+}
+
+function runProject() {
+    const url = 'https://ghibliapi.herokuapp.com/films';    
+
+    fetch(url)
+    .then(response => response.json())
+    .then(allProducts => {
+        allProducts.forEach(movie => renderProducts(movie));
+    })
+}
